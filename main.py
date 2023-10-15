62553ac1846d7d9406237827316ba150f08e10ef
philihoffi <philipp.h@o2online.de>
2023-10-15 17:54:26 +0200

    adding new Tags To DataBase

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from datetime import datetime
 
 import requests
+import psycopg2
+import json
 
-#log filerun in log file which appends the current date and time
+# log filerun in log file which appends the current date and time
 
 write_log = open("log.txt", "a")
-write_log.write("start:"+datetime.now().strftime("%d/%m/%Y %H:%M:%S")+"\t")
+write_log.write("\nstart:" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+
+# read json file
+with open('settings.json') as json_file:
+    data = json.load(json_file)
+
+# Postgresql connection
+print("Connecting to database")
+conn = psycopg2.connect(
+    host=data['database']['host'],
+    port=data['database']['port'],
+    database=data['database']['database'],
+    user=data['database']['user'],
+    password=data['database']['password']
+)
+cursor = conn.cursor()
+print("Connected to database")
 
 # URL der API
 api_url = 'https://www.tagesschau.de/api2/news/'
@@ -13,7 +31,7 @@ api_url = 'https://www.tagesschau.de/api2/news/'
 allNews = {}
 
 while api_url != None:
-    print(f"Anfrage an {api_url}")
+    print(f"Request {api_url}")
 
     # Senden Sie eine GET-Anfrage
     response = requests.get(api_url)
@@ -33,5 +51,24 @@ while api_url != None:
     except KeyError:
         break
 
-write_log.write("end:"+datetime.now().strftime("%d/%m/%Y %H:%M:%S")+"\tNews:"+str(len(allNews))+"\n")
+#insert new Tags
+SelectQuery = "select * from \"Tags\";"
+cursor.execute(SelectQuery)
+tagsInDataBase = [tupel[0] for tupel in cursor.fetchall()]
+
+counter = 0
+insertQuery = "INSERT INTO \"Tags\" (\"name\") VALUES (%s) ON CONFLICT DO NOTHING;"
+for news in allNews.values():
+    for tag in news['tags']:
+        if tag['tag'].lower() in tagsInDataBase:
+            continue
+        cursor.execute(insertQuery, (tag['tag'].lower(),))
+        conn.commit()
+        counter += 1
+
+write_log.write("New Tags: " + str(counter) + "\n")
+
+conn.close()
+
+write_log.write("end:" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\tNews:" + str(len(allNews)) + "\n")
 print(len(allNews))
