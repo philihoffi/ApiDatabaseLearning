e63e60af381270915c39886e2ebe36b0a81ee85d
philihoffi <philipp.h@o2online.de>
2024-07-11 19:44:22 +0200

    parallel

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 from datetime import datetime
-
 import os
-
-try:
-    import requests
-except ImportError:
-    os.system('python -m pip install requests')
 import requests
-
-try:
-    import psycopg2
-except ImportError:
-    os.system('python -m pip install psycopg2')
 import psycopg2
-
-try:
-    import json
-except ImportError:
-    os.system('python -m pip install json')
 import json
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+import time
 
-# log filerun in log file which appends the current date and time
-
+# log file run in log file which appends the current date and time
 write_log = open("log.txt", "a")
 start_time = datetime.now()
 write_log.write("\nstart:" + start_time.strftime("%d/%m/%Y %H:%M:%S") + "\n")
@@ -43,25 +29,42 @@ cursor = conn.cursor()
 print("Connected to database")
 
 # URL der API
-api_url = 'https://www.tagesschau.de/api2/news/'
+api_url = 'https://www.tagesschau.de/api2u/news/'
 
 allNews = {}
 
-SelectQuery = "select \"sophoraId\" from \"News\";"
+SelectQuery = 'SELECT "sophoraId" FROM "News";'
 cursor.execute(SelectQuery)
 newsInDatabaseSopho = [tupel[0] for tupel in cursor.fetchall()]
 
 counter = 0
-while api_url != None:
+MAX_RETRIES = 5
+RETRY_DELAY = 5  # seconds
+
+
+def fetch_data(url):
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.ConnectionError as e:
+            retries += 1
+            print(f"Connection error: {e}. Retrying {retries}/{MAX_RETRIES}...")
+            time.sleep(RETRY_DELAY)
+        except requests.exceptions.RequestException as e:
+            print(f"Request error: {e}. Aborting.")
+            break
+    return None
+
+
+while api_url is not None:
     print(f"Request {api_url}")
 
-    # Senden Sie eine GET-Anfrage
-    response = requests.get(api_url)
+    data = fetch_data(api_url)
 
-    # Überprüfen Sie, ob die Anfrage erfolgreich war (Status-Code 200)
-    if response.status_code == 200:
-        data = response.json()  # Die Antwort als JSON interpretieren
-
+    if data:
         for news in data['news']:
             if news.get('type') == 'video':
                 continue
@@ -69,102 +72,101 @@ while api_url != None:
                 continue
             if news.get('type') != 'video':
                 allNews[news['sophoraId']] = news
-
     else:
-        print(f"Fehler: {response.status_code}")
+        print("Failed to fetch data after multiple retries.")
         break
 
-    try:
-        api_url = data['nextPage']
-    except KeyError:
-        break
-
+    api_url = data.get('nextPage') if data else None
     counter += 1
-    if counter > 7:
+    if counter > 15:
         break
 
 print("start inserting")
 
 # tagsInDataBase
-SelectQuery = "select * from \"Tags\";"
+SelectQuery = 'SELECT * FROM "Tags";'
 cursor.execute(SelectQuery)
 tagsInDataBase = [tupel[0] for tupel in cursor.fetchall()]
 
 # typesInDataBase
-SelectQuery = "select * from \"Types\";"
+SelectQuery = 'SELECT * FROM "Types";'
 cursor.execute(SelectQuery)
 typesInDataBase = [tupel[0] for tupel in cursor.fetchall()]
 
 # tagsToNewsInDataBase
-SelectQuery = "select * from \"Tags_News\";"
+SelectQuery = 'SELECT * FROM "Tags_News";'
 cursor.execute(SelectQuery)
 tagsToNewsInDataBase = [tupel[0] for tupel in cursor.fetchall()]
 
 write_log.write("Adding " + str(len(allNews)) + " news to database\n")
-counter = 0
-for currentNews in allNews:
-    counter += 1
-    print(f"Inserting {counter} of {len(allNews)} types {currentNews}")
-    news = allNews[currentNews]
-    # insert new Tags
-    insertQuery = "INSERT INTO \"Tags\" (\"name\") VALUES (%s) ON CONFLICT DO NOTHING;"
-    for tag in news['tags']:
-        try:
+
+lock = Lock()
+submitted_counter = 0
+
+
+def insert_news(news, index, total):
+    global submitted_counter
+    sophoraId = news['sophoraId']
+    externalId = news['externalId']
+    title = news['title']
+    date = news['date']
+    updateCheckUrl = news['updateCheckUrl']
+    updateCheckUrlJSON = requests.get(news['updateCheckUrl']).text
+    breakingNews = str(news['breakingNews'])
+    topline = news['topline']
+    details = news['details']
+    detailsJSON = requests.get(news['details']).text
+    detailsweb = news['detailsweb']
+    detailswebPage = requests.get(news['detailsweb']).text
+    shareURL = news['shareURL']
+    shareURLPage = requests.get(news['shareURL']).text
+    type = news['type'].lower()
+
+    try:
+        # insert new Tags
+        insertQuery = 'INSERT INTO "Tags" ("name") VALUES (%s) ON CONFLICT DO NOTHING;'
+        for tag in news['tags']:
             cursor.execute(insertQuery, (tag['tag'].lower(),))
             conn.commit()
-        except Exception as e:
-            print("ERROR" + str(e))
-            write_log.write("ERROR" + str(e) + "\n")
 
-    # insert new Types
-    insertQuery = "INSERT INTO \"Types\" (\"name\") VALUES (%s) ON CONFLICT DO NOTHING;"
-    if news['type'].lower() not in typesInDataBase:
-        try:
-            cursor.execute(insertQuery, (news['type'].lower(),))
+        # insert new Types
+        insertQuery = 'INSERT INTO "Types" ("name") VALUES (%s) ON CONFLICT DO NOTHING;'
+        if type not in typesInDataBase:
+            cursor.execute(insertQuery, (type,))
             conn.commit()
-        except Exception as e:
-            print("ERROR" + str(e))
-            write_log.write("ERROR" + str(e) + "\n")
 
-    # Insert new News
-    insertQuery = """
-        INSERT INTO "News"("sophoraId", "externalId", "title", date, "updateCheckUrl", "updateCheckUrlJSON", "breakingNews","topline", "details", "detailsJSON", "detailsweb", "detailswebPage", "shareURL", "shareURLPage", "fk_Type")
-        VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)
-        ON CONFLICT DO NOTHING;
-    """
-    try:
-        sophoraId = news['sophoraId']
-        externalId = news['externalId']
-        title = news['title']
-        date = news['date']
-        updateCheckUrl = news['updateCheckUrl']
-        updateCheckUrlJSON = requests.get(news['updateCheckUrl']).text
-        breakingNews = str(news['breakingNews'])
-        topline = news['topline']
-        details = news['details']
-        detailsJSON = requests.get(news['details']).text
-        detailsweb = news['detailsweb']
-        detailswebPage = requests.get(news['detailsweb']).text
-        shareURL = news['shareURL']
-        shareURLPage = requests.get(news['shareURL']).text
-        type = news['type'].lower()
-
+        # Insert new News
+        insertQuery = '''
+            INSERT INTO "News" ("sophoraId", "externalId", "title", date, "updateCheckUrl", "updateCheckUrlJSON", "breakingNews", "topline", "details", "detailsJSON", "detailsweb", "detailswebPage", "shareURL", "shareURLPage", "fk_Type")
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT DO NOTHING;
+        '''
         cursor.execute(insertQuery, (
         sophoraId, externalId, title, date, updateCheckUrl, updateCheckUrlJSON, breakingNews, topline, details,
         detailsJSON, detailsweb, detailswebPage, shareURL, shareURLPage, type))
         conn.commit()
+
+        # Insert new TagsToNews
+        insertQuery = 'INSERT INTO "Tags_News" ("Tags_name", "News_sophoraId") VALUES (%s, %s) ON CONFLICT DO NOTHING;'
+        for tag in news['tags']:
+            cursor.execute(insertQuery, (tag['tag'].lower(), sophoraId))
+            conn.commit()
+
+        with lock:
+            submitted_counter += 1
+            print(f"Inserting {index} of {total} types {sophoraId}")
+
     except Exception as e:
         print("ERROR" + str(e))
         write_log.write("ERROR" + str(e) + "\n")
 
-    # Insert new TagsToNews
-    insertQuery = "INSERT INTO \"Tags_News\" (\"Tags_name\", \"News_sophoraId\") VALUES (%s, %s) ON CONFLICT DO NOTHING;"
-    for tag in news['tags']:
-        try:
-            cursor.execute(insertQuery, (tag['tag'].lower(), news['sophoraId']))
-            conn.commit()
-        except Exception as e:
-            print("ERROR" + str(e))
-            write_log.write("ERROR" + str(e))
 
-write_log.write("finished:" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\tRuntime: " + str(datetime.now() - start_time) + "\n")
+total_news = len(allNews)
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for index, currentNews in enumerate(allNews.values(), start=1):
+        executor.submit(insert_news, currentNews, index, total_news)
+
+write_log.write("finished:" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\tRuntime: " + str(
+    datetime.now() - start_time) + "\n")
+write_log.close()
+conn.close()
